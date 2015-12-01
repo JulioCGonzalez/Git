0fa6fc1d2d9deb8e21a64ff550e236730a8da17a
JulioCGonzalez <jcgonzalez_95@hotmail.com>
2015-11-30 18:34:50 -0600

    Project

--- Project 1/start.js
+++ Project 1/start.js
@@ -1,6 +1,16 @@
+var indicador = 0;
 $(document).on('ready',function(){
+    $('.left').on('click',function(e){
+        e.preventDefault();
+        moveSlider('left');
+    });
+    $('.right').on('click',function(e){
+        e.preventDefault();
+        moveSlider('right');
+    });
     defineSizes();
 })
+$(window).on('resize',defineSizes);     
 function defineSizes(){
     $('.form_container.slide').each(function(i,el){
         $(el).css({
@@ -8,5 +18,18 @@ function defineSizes(){
             'height': ($('.form_container').width() * 0.45)+'px',
             'with': ($('.form_container').width()) + 'px'
         });
-    });      
+    });  
+     $('.form_container .slideContainer').css({
+       'margin-left' : -(indicador * $('.form_container')) 
+    });
+}
+function moveSlider(direccion){
+    var limite = $('.form_container .slide').length;
+    indicador = (direccion == 'right') ? indicador + 1 : indicador - 1;
+    indicador = (indicador >= limite) ? 0 : indicador;
+    indicador = (indicador < 0) ? limite - 1: indicador;
+    
+    $('.form_container .slideContainer').animate({
+       'margin-left' : -(indicador * $('.form_container')) 
+    });
 }
