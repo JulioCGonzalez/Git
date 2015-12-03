164673500105cea08503b264b7b836e280a87613
JulioCGonzalez <jcgonzalez_95@hotmail.com>
2015-12-03 10:41:19 -0600

    Project

--- Project 1/js/application.js
+++ Project 1/js/application.js
@@ -5,7 +5,7 @@ if (!users) {
 }
 
 
-function saveToLocalStorage(username, password) {
+function saveToLocalStorage(name,lastname,username, password,email) {
 	var user = {
 		"name": name,
         "lastname": lastname,
@@ -35,6 +35,29 @@ function loadUsers() {
 
 	$('#users_table').html(user_html);
 
+}
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var u = users;    
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == u.username){
+alert ("Login successfully");
+window.location = "start_logueado"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
 }
 
 
