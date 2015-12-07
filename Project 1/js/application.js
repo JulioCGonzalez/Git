686978f7554201a6a8f1188a28d1a4a4cdf02772
JulioCGonzalez <jcgonzalez_95@hotmail.com>
2015-12-07 02:04:48 -0600

    Projecto

--- Project 1/js/application.js
+++ Project 1/js/application.js
@@ -1,3 +1,7 @@
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
 $('#save_client').click(function() {
   console.log('Sign_up');
   var mail = document.getElementById("email").value;
@@ -16,6 +20,8 @@ $('#save_client').click(function() {
   window.location = "log_in.html"
 
 });
+//////////////////////
+//////////////////////
 $('#iniciar').click(function() {
 
   var mail = document.getElementById("email").value;
@@ -27,47 +33,50 @@ $('#iniciar').click(function() {
   }
   window.alert("Wrong password or username");
 });
-
+//////////////////////
+//////////////////////
+//////////////////////
+//Envia los correos
 $('#enviar_correo').click(function() {
-  var destino = document.getElementById("correo_destino").value;
-  var asunto = document.getElementById("asunto").value;
-  var contenido = CKEDITOR.instances.editor.getData();
+  var addres = document.getElementById("correo_destino").value;
+  var topic = document.getElementById("asunto").value;
+  var contents = CKEDITOR.instances.editor.getData();
   var enviados = getEnviados();
   var id = enviados.length + 1;
-  var correo = {
+  var mail = {
     "id": id,
-    "destino": destino,
-    "asunto": asunto,
-    "contenido": contenido,
-    "fecha": new Date()
+    "addres": addres,
+    "topic": topic,
+    "contents": contents,
+    "date": new Date()
   };
-  if (destino != "" && asunto != "" && contenido != "") {
-    enviados.push(correo);
+  if (addres != "" && topic != "" && contents != "") {
+    enviados.push(mail);
     setEnviados(enviados);
 
   }
 });
-
+//Guarda los correos
 $('#guardar_correo').click(function() {
-  var destino = document.getElementById("correo_destino").value;
-  var asunto = document.getElementById("asunto").value;
-  var contenido = CKEDITOR.instances.editor.getData();
+  var addres = document.getElementById("correo_destino").value;
+  var topic = document.getElementById("asunto").value;
+  var contents = CKEDITOR.instances.editor.getData();
   var salida = getSalida();
   var id = salida.length + 1;
-  var correo = {
+  var mail = {
     "id": id,
-    "destino": destino,
-    "asunto": asunto,
-    "contenido": contenido,
-    "fecha": new Date()
+    "addres": addres,
+    "topic": topic,
+    "contents": contents,
+    "date": new Date()
   };
-  if (destino != "" && asunto != "" && contenido != "") {
-    salida.push(correo);
+  if (addres != "" && topic != "" && contents != "") {
+    salida.push(mail);
     setSalida(salida);
 
   }
 });
-
+//Get y set de enviado 
 function getEnviados() {
   var enviados = JSON.parse(localStorage.getItem('enviados'));
   return enviados ? enviados : [];
@@ -77,7 +86,7 @@ function setEnviados(datos) {
   localStorage.setItem('enviados', JSON.stringify(
     datos));
 }
-
+//Get y set de salida
 function getSalida() {
   var salida = JSON.parse(localStorage.getItem('salida'));
   return salida ? salida : [];
@@ -87,9 +96,7 @@ function setSalida(datos) {
   localStorage.setItem('salida', JSON.stringify(
     datos));
 }
-
-
-
+//Imprimir Enviados
 function imprimirEnviados() {
   var enviados = getEnviados();
   var tableBody = $('#tEnviados').find('tbody');
@@ -97,10 +104,10 @@ function imprimirEnviados() {
   enviados.forEach(function(enviado, index, array) {
     body +=
       '<tr>' +
-      '<td id="b1">' + enviado.destino + '</td>' +
-      '<td id="b1">' + enviado.asunto + '</td>' +
-      '<td id="b1">' + enviado.contenido + '</td>' +
-      '<td id="b1">' + enviado.fecha + '</td>' +
+      '<td id="b1">' + enviado.addres + '</td>' +
+      '<td id="b1">' + enviado.topic + '</td>' +
+      '<td id="b1">' + enviado.contents + '</td>' +
+      '<td id="b1">' + enviado.date + '</td>' +
       '<td id="b1"><button id="eliminar_correo_enviados" data-id="' +
       enviado.id + '" class="">Eliminar</button></td></tr>';
   });
@@ -110,6 +117,7 @@ function imprimirEnviados() {
 
 }
 
+//Imprimir la salida
 function imprimirSalida() {
   var salida = getSalida();
   var tableBody = $('#tSalida').find('tbody');
@@ -117,10 +125,10 @@ function imprimirSalida() {
   salida.forEach(function(salida, index, array) {
     body +=
       '<tr>' +
-      '<td id="b1">' + salida.destino + '</td>' +
-      '<td id="b1">' + salida.asunto + '</td>' +
-      '<td id="b1">' + salida.contenido + '</td>' +
-      '<td id="b1">' + salida.fecha + '</td>' +
+      '<td id="b1">' + salida.addres + '</td>' +
+      '<td id="b1">' + salida.topic + '</td>' +
+      '<td id="b1">' + salida.contents + '</td>' +
+      '<td id="b1">' + salida.date + '</td>' +
       '<td id="b1"><button id="editar_correo_salida" data-id="' + salida.id +
       '" class="">Editar</button><button id="eliminar_correo_salida" data-id="' +
       salida.id + '" class="">Eliminar</button></td></tr>';
@@ -130,25 +138,7 @@ function imprimirSalida() {
   tableBody.append(body);
 }
 
-
-$(document).delegate("#editar_correo_salida", "click", function() {
-  var id = $(this).data('id');
-  location = location = "editar_correo.html?id=" + id;
-});
-
-$(document).delegate('#eliminar_correo_salida', "click", function() {
-  var id = $(this).data('id');
-  var salida = getSalida();
-  var datos = [];
-  salida.forEach(function(element, index) {
-    if (element.id != id) {
-      datos.push(element);
-    }
-  });
-  setSalida(datos);
-  imprimirSalida();
-});
-
+//Eliminar enviado
 $(document).delegate('#eliminar_correo_enviados', "click", function() {
   var id = $(this).data('id');
   var enviados = getEnviados();
@@ -162,12 +152,31 @@ $(document).delegate('#eliminar_correo_enviados', "click", function() {
   imprimirEnviados();
 });
 
+//Editar salida
+$(document).delegate("#editar_correo_salida", "click", function() {
+  var id = $(this).data('id');
+  location = location = "editar_correo.html?id=" + id;
+});
+//Eliminar salida
+$(document).delegate('#eliminar_correo_salida', "click", function() {
+  var id = $(this).data('id');
+  var salida = getSalida();
+  var datos = [];
+  salida.forEach(function(element, index) {
+    if (element.id != id) {
+      datos.push(element);
+    }
+  });
+  setSalida(datos);
+  imprimirSalida();
+});
+//Editar el de salida
 function setEditarData() {
 
   var salida = getSalida();
   var id = window.location.search.split('=')[1];
-  var destino = document.getElementById('destino');
-  var asunto = document.getElementById('asunto');
+  var addres = document.getElementById('addres');
+  var topic = document.getElementById('topic');
   var editor = CKEDITOR.instances.editor;
   var datos;
 
@@ -176,69 +185,8 @@ function setEditarData() {
       return datos = element;
     }
   });
-  destino.value = datos.destino;
-  asunto.value = datos.asunto;
-  editor.setData(datos.contenido);
+  addres.value = datos.addres;
+  topic.value = datos.topic;
+  editor.setData(datos.contents);
 
 }
-//var users = JSON.parse(localStorage.getItem('users'));
-
-//if (!users) {
-//	users = [];
-//}
-
-
-//function saveToLocalStorage(name, lastname, username, password, email) {
-//	var user = {
-//		"name": name,
-//        "lastname": lastname,
-//        "username": username,
-//        "password": password,
-//        "email": email
-        
-//	};
-//	users.push(user);
-
-//	localStorage.setItem('users', JSON.stringify(users));
-//	loadUsers();
-//}
-
-
-//function loadUsers() {
-
-	// read users from localstorage
-	// loop users
-//	var user_html = "";
-//	for (var i = 0; i < users.length; i++) {
-		// add users to the table
-//		var u = users[i];
-//		user_html = user_html + "<tr><td>"+u.name+"<tr><td>"+u.lastname+"<tr><td>"+u.username+"</td><td>"+
-//		u.password+"</td></tr>"+u.email;
-//	}
-
-//	$('#users_table').html(user_html);
-
-//}
-//var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-//function validate(){
-//var u = users;    
-//var username = document.getElementById("username").value;
-//var password = document.getElementById("password").value;
-//if ( username == u.username && password == u.password){
-//alert ("Login successfully");
-//window.location = "start_logueado"; // Redirecting to other page.
-//return false;
-//}
-//else{
-//attempt --;// Decrementing by one.
-//alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-//if( attempt == 0){
-//document.getElementById("username").disabled = true;
-//document.getElementById("password").disabled = true;
-//document.getElementById("submit").disabled = true;
-//return false;
-//}
-//}
-//}
